b500b37f2bd496ef68d5f014b6e5ff559a5c33f9
Jos Koelewijn <joskw@live.com>
2021-12-06 08:15:11 +0100

    add solution day 6 part 1

--- day6/day6-1.py
+++ day6/day6-1.py
@@ -5,6 +5,20 @@ import os
 currentWorkingDir = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-with open(currentWorkingDir + '\input.txt') as f:
+with open(currentWorkingDir + '\exampleinput.txt') as f:
     lines = f.readlines()
+
+lantarnFish = list(map(int, lines[0].split(',')))
+
+days = 0
+while days < 256:
+    for l in range(0, len(lantarnFish)):
+        if lantarnFish[l] == 0:
+            lantarnFish[l] = 6
+            lantarnFish.append(8)
+        else:
+            lantarnFish[l] -= 1
+    days += 1
+
+print(len(lantarnFish))
 

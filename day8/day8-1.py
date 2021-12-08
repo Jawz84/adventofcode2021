d6160d873c8e2baeaa89e980cc9040f4ba8d5234
Jos Koelewijn <joskw@live.com>
2021-12-08 09:51:59 +0100

    add solution day 8 part 1

--- day8/day8-1.py
+++ day8/day8-1.py
@@ -8,3 +8,10 @@ currentWorkingDir = os.path.realpath(
 with open(currentWorkingDir + '\input.txt') as f:
     lines = f.readlines()
 
+count =0
+for line in lines:
+    for x in line.split('|')[1].strip().split(' '):
+        if len(x) == 2 or len(x) == 3 or len(x) == 4 or len(x) == 7:
+            count += 1
+
+print(count)
